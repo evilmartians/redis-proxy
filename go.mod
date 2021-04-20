54482eff37bcaeb8c82ddb9dfd9c1089e368b47a
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-04-21 00:46:22 +0300

    spike: initial proxy design

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/moonactive/ma-redis-proxy
 go 1.16
 
 require (
+	github.com/go-redis/redis/v8 v8.8.2
 	github.com/heetch/confita v0.10.0
 	github.com/secmask/go-redisproto v0.1.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.7.0
 	github.com/syossan27/tebata v0.0.0-20180602121909-b283fe4bc5ba
 )
--- pkg/multiproxy/session.go
+++ pkg/multiproxy/session.go
@@ -2,18 +2,125 @@ package multiproxy
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"strings"
 
+	"github.com/moonactive/ma-redis-proxy/pkg/redis"
 	"github.com/secmask/go-redisproto"
 )
+
+type sessionState interface {
+	handle(s *Session, cmd *redis.Command) (error, sessionState)
+}
+
+type regularState struct{}
+
+func (st regularState) handle(s *Session, cmd *redis.Command) (error, sessionState) {
+	if cmd.Name == "SELECT" {
+		err := s.writer.WriteError("ERR re-selecting database is not allowed")
+		if err != nil {
+			return err, nil
+		}
+		s.writer.Flush()
+		return fmt.Errorf("SELECT is called after handshake"), nil
+	}
+
+	response, err := s.rdb.Execute(cmd)
+
+	if err != nil {
+		return err, nil
+	}
+
+	// TODO: implement pipeline
+	// if cmd.Last {
+	_, err = s.writer.Write([]byte(response + "\r\n"))
+
+	if err != nil {
+		return err, nil
+	}
+
+	err = s.writer.Flush()
+	if err != nil {
+		return err, nil
+	}
+
+	return nil, nil
+}
+
+type handshakeState struct{}
+
+func (st handshakeState) handle(s *Session, cmd *redis.Command) (error, sessionState) {
+	if cmd.Name != "SELECT" {
+		err := s.writer.WriteError("ERR command is called before `select`")
+		if err != nil {
+			return err, nil
+		}
+
+		err = s.writer.Flush()
+		if err != nil {
+			return err, nil
+		}
+
+		return fmt.Errorf("Command called before SELECT: %s", cmd.Name), nil
+	}
+
+	db := cmd.Args[0].(string)
+	client := s.proxy.LookupClient(db)
+
+	if client == nil {
+		return fmt.Errorf("Database not found: %s", db), nil
+	}
+
+	s.dbname = db
+	s.rdb = client
+
+	err := s.writer.WriteSimpleString("OK")
+
+	if err != nil {
+		return err, nil
+	}
+
+	if cmd.Last {
+		s.writer.Flush()
+	}
+
+	return nil, regularState{}
+}
 
 // Session contains the reference to the target Redis pool,
 // underlying IO object, and is responsible for reading and writing data
 type Session struct {
-	io io.ReadWriter
+	io     io.ReadWriter
+	dbname string
+	rdb    redis.RedisClient
+
+	parser *redisproto.Parser
+	writer *redisproto.Writer
+
+	proxy *Proxy
+	state sessionState
 }
 
-// HandleCommands reads commands from the client's IO, parses them,
+func NewSession(io io.ReadWriter, p *Proxy) *Session {
+	parser := redisproto.NewParser(io)
+	writer := redisproto.NewWriter(bufio.NewWriter(io))
+
+	return &Session{io: io, parser: parser, writer: writer, proxy: p, state: handshakeState{}}
+}
+
+// HandleCommands continuosly reads and executes commands from IO
+func (s *Session) HandleCommands() error {
+	for {
+		err := s.HandleCommand()
+
+		if err != nil {
+			return err
+		}
+	}
+}
+
+// HandleCommand reads a command from the client's IO, parses it,
 // passes to the Redis client and writes the response back.
 // It is also responsible to treat some commands differently:
 //    - SELECT — used to route a client to the specified database pool
@@ -22,33 +129,64 @@ type Session struct {
 //    - CLIENT — client state commands (e.g., SETNAME)
 //    - MULTI/EXEC — accumulate commands and wait for exec to process them together
 // TODO: To be implemented
-func (s *Session) HandleCommands() error {
-	parser := redisproto.NewParser(s.io)
-	writer := redisproto.NewWriter(bufio.NewWriter(s.io))
+func (s *Session) HandleCommand() error {
+	command, err := s.readCommand()
 
-	for {
-		command, err := parser.ReadCommand()
-		if err != nil {
-			_, ok := err.(*redisproto.ProtocolError)
-			if ok {
-				err = writer.WriteError(err.Error())
-				if err != nil {
-					return err
-				}
-			} else {
-				return err
-			}
-			continue
-		}
-
-		err = writer.WriteBulkString("OK")
-
-		if command.IsLast() {
-			writer.Flush()
-		}
-
-		if err != nil {
-			return err
-		}
+	if err != nil {
+		return err
 	}
+
+	// Protocol-level error has been already handled
+	if command == nil {
+		return nil
+	}
+
+	// Handle commands which do not require calling a real Redis
+	if handler, ok := fastlane[command.Name]; ok {
+		return handler(s, command)
+	}
+
+	err, newState := s.state.handle(s, command)
+
+	if err != nil {
+		return err
+	}
+
+	if newState != nil {
+		s.state = newState
+	}
+
+	return nil
+}
+
+func (s *Session) readCommand() (*redis.Command, error) {
+	protoCmd, err := s.parser.ReadCommand()
+	if err != nil {
+		_, ok := err.(*redisproto.ProtocolError)
+		if ok {
+			err = s.writer.WriteError(err.Error())
+			if err != nil {
+				return nil, err
+			}
+			s.writer.Flush()
+		} else {
+			return nil, err
+		}
+
+		return nil, nil
+	}
+
+	restArgsCount := protoCmd.ArgCount() - 1
+
+	command := redis.Command{
+		Name: strings.ToUpper(string(protoCmd.Get(0))),
+		Last: protoCmd.IsLast(),
+		Args: make([]interface{}, restArgsCount),
+	}
+
+	for i := 1; i < protoCmd.ArgCount(); i++ {
+		command.Args[i-1] = string(protoCmd.Get(i))
+	}
+
+	return &command, nil
 }
